2be987236787850a0174f7f3e88b2af37d7e6592
Lee Dongryeol <dr8547@naver.com>
2023-01-12 00:02:31 +0900

    영속성 관리 - 내부 동작 방식

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -24,15 +24,49 @@ public class JpaMain {
             // DB에서 트랜잭션이란 단위는 중요하다
             //em.persist(findMember);
 
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+            /*List<Member> result = em.createQuery("select m from Member as m", Member.class)
                     .setFirstResult(1)
                     .setMaxResults(10)
                     .getResultList();
 
             for (Member member: result) {
                 System.out.println("member.getName() = " + member.getName());
-            }
-            
+            }*/
+
+/*            // 비영속
+            Member member = new Member();
+            member.setId(101L);
+            member.setName("HelloJPA");
+
+            // 영속
+            System.out.println("=== BEFORE ===");
+            em.persist(member);
+            System.out.println("=== AFTER ===");
+
+            Member findMember = em.find(Member.class, 101L);
+
+            System.out.println("findMember.id = " + findMember.getId());
+            System.out.println("findMember.name = " + findMember.getName());*/
+
+            /*Member findMember1 = em.find(Member.class, 101L);
+            Member findMember2 = em.find(Member.class, 101L);*/
+
+            // 영속
+/*            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(160L, "B");
+
+            em.persist(member1);
+            em.persist(member2);
+
+            System.out.println("==================");*/
+
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZ");
+
+            em.flush();
+
+            System.out.println("==================");
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
